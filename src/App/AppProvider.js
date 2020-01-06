3a3274657ae2f4922c7fc5cdaa0957809d8f0de5
Denis Rebenok <denis.rebenok.pro@gmail.com>
2020-01-06 15:29:10 +0200

    Settings page, localStorage

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -3,10 +3,25 @@ import React, {useState, createContext} from 'react'
 export const AppContext = createContext()
 
 export default function AppProvider(props) {
-  const [page, setPage] = useState('dashboard')
+  const [page, setPage] = useState('settings')
+  const [firstVisit, setFirstVisit] = useState(true)
+
+  const savedSettings = () => {
+    let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'))
+    if (!cryptoDashData) {
+      setPage('settings')
+      setFirstVisit(true)
+    }
+  }
+
+  const confirmFavorites = () => {
+    setFirstVisit(false)
+    setPage('dashboard')
+    localStorage.setItem('cryptoDash', JSON.stringify({test: 'hello'}))
+  }
 
   return (
-    <AppContext.Provider value={{page, setPage}}>
+    <AppContext.Provider value={{page, setPage, savedSettings, confirmFavorites, firstVisit}}>
       {props.children}
     </AppContext.Provider>
   )
--- src/App/index.js
+++ src/App/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage'
+
 import AppLayout from './AppLayout'
 import AppBar from './AppBar'
 import AppProvider from './AppProvider'
-// import { AppContext } from "./AppProvider";
+import Settings from '../Settings/index'
 
 export default function App() {
   return (
     <AppLayout>
       <AppProvider>
         <AppBar />
-        <WelcomeMessage />
+        <Settings />
       </AppProvider>
     </AppLayout>
   )
